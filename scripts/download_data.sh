4aa91bfd1b30fa4acf15e1bb2496cfeedf16ca93
Maciej Urbanski <rooter@kyberian.net>
2021-08-17 10:56:00 +0200

    fix download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -5,7 +5,7 @@ mkdir -p "$TARGET_DIR"
 
 shift
 for name in "$@" ; do
-  URL="http://gem.cs.ru.nl/{$name}.tar.gz"
+  URL="http://gem.cs.ru.nl/${name}.tar.gz"
   echo Fetching $URL
   curl "$URL" | tar xz --directory "$TARGET_DIR"
 done
